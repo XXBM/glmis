0c32872f1950a840d6d752297d92fd3992b2555e
XXBM <1961346638@qq.com>
2017-10-10 19:02:20 +0800

    menu

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,46 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 //For router-use
-
-
+import homePage from '../components/homePage.vue'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
+    	{
+        path: '/personnel',
+        name: '首页',
+        component: homePage
+    	},
+    	{
+        path: '/tjcx',
+        name: '首页',
+        component: homePage
+    	},
+    	{
+        path: '/attendanceManager',
+        name: '首页',
+        component: homePage
+    	},
+    	{
+        path: '/attendanceQuery',
+        name: '首页',
+        component: homePage
+    	},
+    	{
+        path: '/attendanceExportExcel',
+        name: '首页',
+        component: homePage
+    	},
+    	{
+        path: '/scientificResearch',
+        name: '首页',
+        component: homePage
+    	},
+    	{
+        path: '/myScientificResearch',
+        name: '首页',
+        component: homePage
+    	}
     ]
 })
