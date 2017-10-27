6621830195e64e1c9e24b8313b850b6e263add5b
zhangzhengqiqi <1574900601@qq.com>
2017-10-27 19:03:04 +0800

    tab

---
+++ src/components/employee/scientificResearch/scientificInfo.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<el-tabs type="card" @tab-click="handleClick">
+		    <el-tab-pane label="论文" name="thesis">论文</el-tab-pane>
+		    <el-tab-pane label="获奖" name="award">获奖</el-tab-pane>
+		    <el-tab-pane label="专著" name="monograph">专著</el-tab-pane>
+		    <el-tab-pane label="教材" name="teachingMaterial">教材</el-tab-pane>
+		    <el-tab-pane label="横向的项目" name="infeedProject">横向的项目</el-tab-pane>
+		    <el-tab-pane label="纵向的项目" name="lengwaysProject">纵向的项目</el-tab-pane>
+	    </el-tabs>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		data(){
+			return{
+				thesisUrl:'',
+				awardUrl:'',
+			}
+		},
+		methods:{
+			handleClick(tab){
+				if(tab.name==thesis){
+					this.thesisUrl="/displayOwnThesis"
+				}else if(tab.name==award){
+					this.awardUrl="/"
+				}else if(tab.name==monograph){
+
+				}else if(tab.name==teachingMaterial){
+
+				}else if(tab.name==infeedProject){
+
+				}else{
+
+				}
+			}
+		}
+	}
+</script>
