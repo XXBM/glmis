61f2cf968c3b5f981bf374206428d9778531c25b
XXBM <1961346638@qq.com>
2017-11-25 19:23:04 +0800

    冲突

--- src/components/scientificResearch/materialForWorker.vue
+++ src/components/scientificResearch/materialForWorker.vue
@@ -6,15 +6,6 @@
     <el-tab-pane label="项目(纵向)" name="lengthways">
     	<project :url="lengthwaysUrl" :workerId="workerId"></project>
     </el-tab-pane>
-    <el-tab-pane label="论文" name="third">角色管理</el-tab-pane>
-    <el-tab-pane label="获奖" name="dd">定时任务补偿</el-tab-pane>
-    <el-tab-pane label="教材" name="fouddrth">定时任务补偿</el-tab-pane>
-    <el-tab-pane label="专著" name="fourssth">定时任务补偿</el-tab-pane>
-    	<project-lengthways :url="projectUrl"></project-lengthways>
-    </el-tab-pane>
-    <el-tab-pane label="项目(纵向)" name="lengthways">
-    	<project :url="lengthwaysUrl"></project>
-    </el-tab-pane>
     <el-tab-pane label="论文" name="thesis">
       <thesis :url="thesisUrl" :workerId="workerId"></thesis>
     </el-tab-pane>
